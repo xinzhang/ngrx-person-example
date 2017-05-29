3b8d1eed1f9d3fef5d1a006cde06ea336caa4c9c
Xin Zhang <Xin.Zhang@ausmaq.com.au>
2017-05-29 11:44:48 +1000

    ngrx example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
 
+import {Store, provideStore} from '@ngrx/store';
+
+import {PersonList} from './components/person-list';
+import {PersonInput} from './components/person-input';
+import {FilterSelect} from './components/filter-select';
+
+import {people} from "./reducers/people";
+import {filter} from "./reducers/filter";
+
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/observable/combineLatest';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  	public people;
+	  private id = 0;
+
+    constructor(	private _store : Store<any>	){
+		this.people = Observable.combineLatest(
+			_store.select('people'),
+			_store.select('filter'),
+			(people : any[], filter:any) => {
+				return people.filter(filter);
+			}
+		)
+    //this.people = _store.select('people');
+	}
+
+	addPerson(name){
+		this._store.dispatch({type: "ADD_PERSON", payload: {
+			id: ++this.id,
+			name,
+			guests: 0,
+			attending: false
+		}})
+	}
+
+	addGuest({id}){
+		this._store.dispatch({type: "ADD_GUESTS", payload: id});
+	}
+
+	removeGuest({id}){
+		this._store.dispatch({type: "REMOVE_GUESTS", payload: id});
+	}
+
+	removePerson({id}){
+		this._store.dispatch({type: "REMOVE_PERSON", payload: id});
+	}
+
+
+	toggleAttending({id}){
+		this._store.dispatch({type: "TOGGLE_ATTENDING", payload: id});
+	}
+
+	updateFilter(filter){
+		this._store.dispatch({type: filter});
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {PersonList} from './components/person-list';
+import {PersonInput} from './components/person-input';
+import {FilterSelect} from './components/filter-select';
+
+import { StoreModule } from '@ngrx/store';
+import {people} from "./reducers/people";
+import {filter} from "./reducers/filter";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,PersonList, PersonInput, FilterSelect
   ],
   imports: [
     BrowserModule,
+    StoreModule.provideStore({ people, filter}),
     FormsModule,
     HttpModule
-  ],
-  providers: [],
+  ],  
+  providers:[],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
